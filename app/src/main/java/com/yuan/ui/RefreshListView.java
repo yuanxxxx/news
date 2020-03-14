257821ec18ab886f6f03125de6e82c987f5f8887
yuan <xieyuansjtu@163.com>
2020-03-14 17:19:23 +0800

    新闻详情页 下拉刷新

--- app/src/main/java/com/yuan/ui/RefreshListView.java
+++ app/src/main/java/com/yuan/ui/RefreshListView.java
@@ -3,6 +3,7 @@ package com.yuan.ui;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -28,6 +29,11 @@ public class RefreshListView extends ListView {
     private LinearLayout ll_header_view;
     private int headerHeight;
 
+
+    private float startX;
+
+    private float startY = -1;
+
     public RefreshListView(Context context) {
         this(context, null);
     }
@@ -42,18 +48,41 @@ public class RefreshListView extends ListView {
     }
 
     private void initHeaderView(Context context) {
-        View ll_header_view = View.inflate(context, R.layout.refresh_header, null);
+        ll_header_view = (LinearLayout) View.inflate(context, R.layout.refresh_header, null);
         tv_refresh_title = (TextView) ll_header_view.findViewById(R.id.tv_refresh_title);
         tv_refresh_time = (TextView) ll_header_view.findViewById(R.id.tv_refresh_time);
         ll_refresh_header = (LinearLayout) ll_header_view.findViewById(R.id.ll_refresh_header);
 
-        ll_header_view.measure(0, 0);
-        headerHeight = ll_header_view.getHeight();
-        ll_header_view.setPadding(0, -headerHeight, 0, 0);
+        ll_refresh_header.measure(0, 0);
+        headerHeight = ll_refresh_header.getMeasuredHeight();
+        ll_refresh_header.setPadding(0, -headerHeight, 0, 0);
 
         this.addHeaderView(ll_header_view);
     }
 
 
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (startY == -1) {
+                    startY = ev.getY();
+                }
+                float endY = ev.getY();
+                float distanceY = endY - startY;
+                if (distanceY > 0) {
+                    ll_refresh_header.setPadding(0, (int) (distanceY - headerHeight), 0, 0);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
 
+        return super.onTouchEvent(ev);
+    }
 }
