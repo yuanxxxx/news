b9d8f0707b8d10eea1665b28e16893e45a17864c
yuan <xieyuansjtu@163.com>
2020-03-17 14:15:24 +0800

    增加String类型缓存

--- app/src/main/java/com/yuan/utils/CacheUtils.java
+++ app/src/main/java/com/yuan/utils/CacheUtils.java
@@ -18,4 +18,15 @@ public class CacheUtils {
         SharedPreferences sp = context.getSharedPreferences("yuan", Context.MODE_PRIVATE);
         sp.edit().putBoolean(key, value).apply();
     }
+
+    public static String getString(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences("yuan", Context.MODE_PRIVATE);
+        return sp.getString(key, "");
+    }
+
+
+    public static void putString(Context context, String key, String value) {
+        SharedPreferences sp = context.getSharedPreferences("yuan", Context.MODE_PRIVATE);
+        sp.edit().putString(key, value).apply();
+    }
 }
